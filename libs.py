f9aff40c24acf050a14ff25fb4bf91b8bf088d73
AlessandroPulcini <79671028+AlessandroPulcini@users.noreply.github.com>
2021-03-05 17:23:55 +0100

    Code Complete
    Added step 3 functions: code is complete and working.
    Coming soon: test and refactor.

--- libs.py
+++ libs.py
@@ -3,7 +3,6 @@ import itertools
 
 def data_structure_check(data_, photo_dim_, categories_):
     """
-
     Parameters:
         data_: the 4-dimensional list containing the raw output
         photo_dim_: original photo dimensions
@@ -53,28 +52,26 @@ def data_structure_check(data_, photo_dim_, categories_):
 
 def determine_class(data_):
     """
-
     Parameters:
         data_: the 4-dimensional list containing the raw output
     Return:
         A 3-dimensional list with the probability, for each NxN square, of containing an object of a specific category
             (chosen as the one giving the highest probability)
     """
-    for Bp, Ap in itertools.product(range(len(data_)), range(len(data_[0]))):
+    for B, A in itertools.product(range(len(data_)), range(len(data_[0]))):
         probability = 0
-        for element in data_[Bp][Ap]:
+        for element in data_[B][A]:
             for i, x in enumerate(element[5:]):
                 if (element[0] * x) > probability:
                     probability = element[0] * x
                     index = i
                     box = element
-        data_[Bp][Ap] = [probability] + box[1:5] + [index + 1]
+        data_[B][A] = [probability] + box[1:5] + [index + 1]
     return data_
 
 
 def coordinate_conversion(data_, N_size):
     """
-
     Parameters:
         data_: The 3-dimensional list containing information about one box per square and coordinated expressed
             in format 1
@@ -82,28 +79,72 @@ def coordinate_conversion(data_, N_size):
     Return:
         A 3-dimensional list like the parameter "data_" with coordinates converted in format 2
     """
-    for Bp, Ap in itertools.product(range(len(data_)), range(len(data_[0]))):
-        data_[Bp][Ap] = [data_[Bp][Ap][0]] + \
-                        [Ap * N_size + data_[Bp][Ap][1] - data_[Bp][Ap][3] / 2] + \
-                        [Bp * N_size + data_[Bp][Ap][2] - data_[Bp][Ap][4] / 2] + \
-                        [Ap * N_size + data_[Bp][Ap][1] + data_[Bp][Ap][3] / 2] + \
-                        [Bp * N_size + data_[Bp][Ap][2] + data_[Bp][Ap][4] / 2] + \
-                        [data_[Bp][Ap][-1]]
+    for B, A in itertools.product(range(len(data_)), range(len(data_[0]))):
+        data_[B][A] = [data_[B][A][0]] + \
+                      [A * N_size + data_[B][A][1] - data_[B][A][3] / 2] + \
+                      [B * N_size + data_[B][A][2] - data_[B][A][4] / 2] + \
+                      [A * N_size + data_[B][A][1] + data_[B][A][3] / 2] + \
+                      [B * N_size + data_[B][A][2] + data_[B][A][4] / 2] + \
+                      [data_[B][A][-1]]
     return data_
 
 
 def filtering(data_, thrsh_=0.5):
     """
-
     Parameters:
         data_: The 3-dimensional list containing information about one box per square and coordinated expressed
             in format 2
         thrsh_: Threshold parameter with default value of 0.5
     Return:
-        A subset of input boxes containing only those with probability greater than or equal to the threshold
+        A subset of input boxes in form of a 2-dimensional array containing only those with probability greater
+        than or equal to the threshold
     """
     filtered_data = []
-    for Bp, Ap in itertools.product(range(len(data_)), range(len(data_[0]))):
-        if data_[Bp][Ap][0] >= thrsh_:
-            filtered_data.append(data_[Bp][Ap])
+    for B, A in itertools.product(range(len(data_)), range(len(data_[0]))):
+        if data_[B][A][0] >= thrsh_:
+            filtered_data.append(data_[B][A])
     return filtered_data
+
+
+def IoU_calculation(box1, box2):
+    """
+    Parameters:
+        box1: List of parameter describing a single box with format 2
+        box2: List of parameter describing a single box with format 2
+    Return:
+        Intersection over union of the two rectangles
+    """
+
+    x_min = max(box1[1], box2[1])
+    y_min = max(box1[2], box2[2])
+    x_max = min(box1[3], box2[3])
+    y_max = min(box1[4], box2[4])
+    if (x_min > x_max) or (y_min > y_max):
+        IoU_value = 0
+    else:
+        I_area = (x_max - x_min) * (y_max - y_min)
+        U_area = (box1[3] - box1[1]) * (box1[4] - box1[2]) + \
+                 (box2[3] - box2[1]) * (box2[4] - box2[2]) - \
+                 I_area
+        IoU_value = I_area / U_area
+
+    return IoU_value
+
+
+def suppression(data_, suppr_ = 0.5):
+    """
+    Parameters:
+        data_: The 3-dimensional list containing information about one box per square and coordinated expressed
+            in format 2
+        suppr_: Threshold parameter with default value of 0.5
+    Return:
+        A subset of input boxes without overlapping boxes
+    """
+    for el1 in data_:
+        for el2 in data_:
+            if IoU_calculation(el1, el2) > suppr_:
+                if el1[0] > el2[0]:
+                    data_.remove(el2)
+                else:
+                    data_.remove(el1)
+    return data_
--- main.py
+++ main.py
@@ -13,18 +13,20 @@ except ImportError:
 # creating a dictionary for categories
 cat_dict = {}
 for ind, cat in enumerate(categories):
-    cat_dict[ind+1] = cat
+    cat_dict[ind + 1] = cat
 
 # Step 1.a: class determination
 data = libs.determine_class(data)
 # Step 1.b: coordinates conversion
 data = libs.coordinate_conversion(data, photo_dim[0] / par_Ap)
-print(data)
+#print(data)
 
 # Step 2: filtering
-if filtering_threshold > 0:
-    filtered_subset = libs.filtering(data, filtering_threshold)
-print(filtered_subset)
+filtered_subset = libs.filtering(data, filtering_threshold)
+#print(filtered_subset)
+
+# Step 3: Non-max suppression
+filtered_subset = libs.suppression(filtered_subset, IoU_threshold)
 
 # Output: List of string stating category detected with given probability (.2f) at location (in format 2)
 output_list = []
@@ -35,3 +37,5 @@ for box in filtered_subset:
                  " at ((" + str(box[1]) + ", " + str(box[2]) + "), (" + \
                  str(box[3]) + ", " + str(box[4]) + "))"
     output_list.append(output_str)
+
+print(output_list)
